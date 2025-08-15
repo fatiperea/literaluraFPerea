903defa3b0e528868ad99aac2d38cb564893c652
fatiperea <fatimaperea3pppp0@gmail.com>
2025-08-15 02:46:53 -0300

    creo clase autor

--- src/main/java/com/perea/literalura/model/Autor.java
+++ src/main/java/com/perea/literalura/model/Autor.java
@@ -2,5 +2,40 @@ package com.perea.literalura.model;
 
 public class Autor {
 
+    private Long id;
+    private String nombre;
+    private String nacimiento;
+    private String fallecimiento;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNacimiento() {
+        return nacimiento;
+    }
+
+    public void setNacimiento(String nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    public String getFallecimiento() {
+        return fallecimiento;
+    }
+
+    public void setFallecimiento(String fallecimiento) {
+        this.fallecimiento = fallecimiento;
+    }
 }
