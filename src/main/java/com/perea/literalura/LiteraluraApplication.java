50182d658a66e92dbcbf0d4cdeed1afbe69c8abc
fatiperea <fatimaperea3pppp0@gmail.com>
2025-08-15 00:34:55 -0300

    agrego aplication properties y el command line runner

--- src/main/java/com/perea/literalura/LiteraluraApplication.java
+++ src/main/java/com/perea/literalura/LiteraluraApplication.java
@@ -1,13 +1,25 @@
 package com.perea.literalura;
 
+import com.perea.literalura.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class LiteraluraApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Principal principal= new Principal();
+
+		principal.menu();
+
 	}
 
 }
