d7d18147ed123d350ad1d18bf6a95f1404c6c651
fatiperea <fatimaperea3pppp0@gmail.com>
2025-08-14 23:32:49 -0300

    creo record datos-autor

--- src/main/java/com/perea/literalura/model/DatosLibro.java
+++ src/main/java/com/perea/literalura/model/DatosLibro.java
@@ -9,7 +9,7 @@ import java.util.List;
 public record DatosLibro(
 
         @JsonAlias("title") String titulo,
-        //@JsonAlias("authors") List<DatosAutor> autor,
+        @JsonAlias("authors") List<DatosAutor> autor,
         @JsonAlias("languages") List<String> idiomas,
         @JsonAlias("download_count") Double numeroDeDescargas
 
